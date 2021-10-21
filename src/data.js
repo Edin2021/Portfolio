2c5ce4eab69a8b45ef049ddf490a0674051915e8
Edin Sikira <edinsikira5@gmail.com>
2021-10-21 21:22:43 +0200

    Added project superchat, updated data.js and resume

--- src/data.js
+++ src/data.js
@@ -1,14 +1,25 @@
-import audiophileImage from "./images/project-images/audiophile-fullwidth.png";
+import audiophileImage from "./images/project-images/audiophile.png";
+import superchatImage from "./images/project-images/superchat.png";
 
 const projects = [
   {
     id: "1",
     image: audiophileImage,
-    techUsed: "Sass/Javascript/ReacjJS",
+    techUsed: "Sass / Javascript / ReacjJS",
     name: "audiophile e-commerce",
     info: "This website contains a checkout page with form validation, a cart and more.",
     repo: "https://github.com/Edin2021/audiophile-ecommerce",
     demo: "https://audiophile-ecommerce.pages.dev/",
+    selected: true,
+  },
+  {
+    id: "2",
+    image: superchatImage,
+    techUsed: "Sass / Javascript / ReacjJS / Firebase",
+    name: "superchat",
+    info: "This app contains a login/sign up system, a group chat and a button to log out.",
+    repo: "https://github.com/Edin2021/superchat",
+    demo: "https://superchat.pages.dev/chat",
     selected: true,
   },
 ];
